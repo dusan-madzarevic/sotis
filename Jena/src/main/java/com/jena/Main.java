e503b6eaf09522041fa4f35c68f20868946ec6a3
dusan-madzarevic <ducom98@gmail.com>
2021-11-02 21:05:18 +0100

    Jena fuseki connection

--- Jena/src/main/java/com/jena/Main.java
+++ Jena/src/main/java/com/jena/Main.java
@@ -1,4 +1,13 @@
 package com.jena;
+
+import org.apache.jena.query.Dataset;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdfconnection.RDFConnectionFuseki;
+import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
+import org.apache.jena.tdb.TDBFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -6,5 +15,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        RDFConnectionRemoteBuilder builder = RDFConnectionFuseki.create()
+                .destination("http://localhost:3030/Education/sparql");
+
+        Query query = QueryFactory.create("SELECT * { BIND('Hello'as ?text) }");
+
+        // In this variation, a connection is built each time.
+        try ( RDFConnectionFuseki conn = (RDFConnectionFuseki)builder.build() ) {
+            conn.queryResultSet(query, ResultSetFormatter::out);
+        }
+
+
     }
 }
